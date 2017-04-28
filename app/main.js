734e0ce09f81501168d845aebf6c7c657eacbfec
Andrew Jenkins <andrew.jenkins@centricient.com>
2017-04-28 09:23:00 -0600

    Conditionally load intl polyfill

--- app/main.js
+++ app/main.js
@@ -1,37 +1,51 @@
 import 'babel-polyfill';
 import React from 'react';
 import Launcher from 'Launcher';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { IntlProvider } from 'react-intl';
+import {render} from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
+import {IntlProvider} from 'react-intl';
 import Redbox from 'redbox-react';
 
 import 'main.scss';
 
-const root = document.createElement('div');
-root.id = 'quiqWebChat'; // If for some reason you change this, make sure you update the webpack config to match it!
-document.getElementsByTagName('body')[0].appendChild(root);
+const init = () => {
+  const root = document.createElement('div');
+  root.id = 'quiqWebChat'; // If for some reason you change this, make sure you update the webpack config to match it!
+  document.getElementsByTagName('body')[0].appendChild(root);
 
-render(
-  <IntlProvider locale="en">
-    <AppContainer errorReporter={Redbox}>
-      <Launcher />
-    </AppContainer>
-  </IntlProvider>,
-  document.getElementById('quiqWebChat')
-);
+  render(
+    <IntlProvider locale="en">
+      <AppContainer errorReporter={Redbox}>
+        <Launcher />
+      </AppContainer>
+    </IntlProvider>,
+    document.getElementById('quiqWebChat'),
+  );
 
-if (module.hot) {
-  module.hot.accept('Launcher', () => {
-    const NextApp = require('Launcher').default; // eslint-disable-line global-require
+  if (module.hot) {
+    module.hot.accept('Launcher', () => {
+      const NextApp = require('Launcher').default; // eslint-disable-line global-require
 
-    render(
-      <IntlProvider locale="en">
-        <AppContainer errorReporter={Redbox}>
-          <NextApp />
-        </AppContainer>
-      </IntlProvider>,
-      document.getElementById('quiqWebChat')
-    );
+      render(
+        <IntlProvider locale="en">
+          <AppContainer errorReporter={Redbox}>
+            <NextApp />
+          </AppContainer>
+        </IntlProvider>,
+        document.getElementById('quiqWebChat'),
+      );
+    });
+  }
+};
+
+// Conditionally load Intl polyfill for old versions of IE
+// TODO: Load correct locale once we add i18n
+if (!global.Intl) {
+  require.ensure(['intl', 'intl/locale-data/jsonp/en.js'], function(require) {
+    require('intl');
+    require('intl/locale-data/jsonp/en.js');
+    init();
   });
+} else {
+  init();
 }
