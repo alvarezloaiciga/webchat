2abf261379d5012b9bd2a6834c8769ad1eff2ebe
Nate Norberg <natenorberg@gmail.com>
2017-05-01 12:39:57 -0600

    Added placeholder when no agents are available

--- app/network/chat.js
+++ app/network/chat.js
@@ -1,23 +1,20 @@
 // @flow
-
 import QUIQ from 'utils/quiq';
 import fetch from 'isomorphic-fetch';
-import type { Conversation } from 'types';
+import type {Conversation} from 'types';
 
-const { HOST, CONTACT_POINT } = QUIQ;
+const {HOST, CONTACT_POINT} = QUIQ;
 
 const parseResponse = (response: Response): Promise<*> => {
   if (response.status && response.status >= 300) {
-    return response.json()
-      .then((res) => Promise.reject(res))
-      .catch((err) => Promise.reject(err));
+    return response.json().then(res => Promise.reject(res)).catch(err => Promise.reject(err));
   }
 
   return response.json();
 };
 
 export const joinChat = () => {
-  fetch(`${ HOST }/api/v1/messaging/chat/${ CONTACT_POINT }/join`, {
+  fetch(`${HOST}/api/v1/messaging/chat/${CONTACT_POINT}/join`, {
     mode: 'cors',
     credentials: 'include',
     method: 'POST',
@@ -29,7 +26,7 @@ export const joinChat = () => {
 };
 
 export const leaveChat = () => {
-  fetch(`${ HOST }/api/v1/messaging/chat/${ CONTACT_POINT }/leave`, {
+  fetch(`${HOST}/api/v1/messaging/chat/${CONTACT_POINT}/leave`, {
     mode: 'cors',
     credentials: 'include',
     method: 'POST',
@@ -41,7 +38,7 @@ export const leaveChat = () => {
 };
 
 export const ping = () => {
-  fetch(`${ HOST }/api/v1/webchat/endpoints/${ CONTACT_POINT }/socket-ping`, {
+  fetch(`${HOST}/api/v1/webchat/endpoints/${CONTACT_POINT}/socket-ping`, {
     mode: 'cors',
     method: 'POST',
     credentials: 'include',
@@ -49,7 +46,7 @@ export const ping = () => {
 };
 
 export const addMessage = (body: string) => {
-  fetch(`${ HOST }/api/v1/messaging/chat/${ CONTACT_POINT }/send-message`, {
+  fetch(`${HOST}/api/v1/messaging/chat/${CONTACT_POINT}/send-message`, {
     mode: 'cors',
     credentials: 'include',
     method: 'POST',
@@ -61,22 +58,20 @@ export const addMessage = (body: string) => {
   });
 };
 
-export const fetchWebsocketInfo = (): Promise<{ url: string }> => (
-  fetch(`${ HOST }/api/v1/messaging/chat/${ CONTACT_POINT }/socket-info`, {
+export const fetchWebsocketInfo = (): Promise<{url: string}> =>
+  fetch(`${HOST}/api/v1/messaging/chat/${CONTACT_POINT}/socket-info`, {
     mode: 'cors',
     credentials: 'include',
-  }).then(parseResponse)
-);
+  }).then(parseResponse);
 
-export const fetchConversation = (): Promise<Conversation> => (
-  fetch(`${ HOST }/api/v1/messaging/chat/${ CONTACT_POINT }`, {
+export const fetchConversation = (): Promise<Conversation> =>
+  fetch(`${HOST}/api/v1/messaging/chat/${CONTACT_POINT}`, {
     mode: 'cors',
     credentials: 'include',
-  }).then(parseResponse)
-);
+  }).then(parseResponse);
 
 export const updateMessagePreview = (body: string, typing: boolean) => {
-  fetch(`${ HOST }/api/v1/messaging/chat/${ CONTACT_POINT }/typing`, {
+  fetch(`${HOST}/api/v1/messaging/chat/${CONTACT_POINT}/typing`, {
     mode: 'cors',
     credentials: 'include',
     method: 'POST',
@@ -84,6 +79,16 @@ export const updateMessagePreview = (body: string, typing: boolean) => {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ body, typing }),
+    body: JSON.stringify({body, typing}),
   });
 };
+
+export const checkForAgents = (): Promise<{available: boolean}> =>
+  fetch(`${HOST}/api/v1/messaging/contact-points/Chat/${CONTACT_POINT}/agents-available`, {
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  }).then(parseResponse);
