6a3b06a28e40edd1a1efb5483e18eb4f603f266e
Nate Norberg <nate.norberg@goquiq.com>
2017-06-02 16:58:45 -0600

    Removed sentry

--- app/main.js
+++ app/main.js
@@ -1,4 +1,3 @@
-/* global __DEV__ __VERSION__ */
 import 'babel-polyfill';
 import {nonCompatibleBrowser} from 'utils/utils';
 import React from 'react';
@@ -7,17 +6,10 @@ import {render} from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 import {IntlProvider} from 'react-intl';
 import Redbox from 'redbox-react';
-import Raven from 'raven-js';
 import QUIQ from 'utils/quiq';
 import {init as initChat} from 'quiq-chat';
 
 import 'main.scss';
-
-if (!__DEV__) {
-  Raven.config('https://5622397f17c44165a039d3b91d3e0193@sentry.io/170102', {
-    release: __VERSION__,
-  }).install();
-}
 
 initChat({
   HOST: QUIQ.HOST,
