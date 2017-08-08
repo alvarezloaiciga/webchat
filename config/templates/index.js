13897734992c78c5870694c947e007fca72e4d76
Andrew Jenkins <andrew.jenkins@centricient.com>
2017-08-08 10:26:31 -0600

    Fix loader

--- config/templates/index.js
+++ config/templates/index.js
@@ -56,27 +56,20 @@ try {
     quiqChatFrame.style.left = window.QUIQ.POSITION.left || quiqChatFrame.style.left;
     quiqChatFrame.style.top = window.QUIQ.POSITION.top || quiqChatFrame.style.top;
     quiqChatFrame.style.border = 'none';
-    document.body.appendChild(quiqChatFrame);
-
-    var onLoadCallback = function() {
-      quiqChatFrame.contentWindow.postMessage({ QUIQ: window.QUIQ }, href);
+    quiqChatFrame.onload = function() {
+      this.contentWindow.postMessage({ QUIQ: window.QUIQ }, href);
     };
 
-    if(document.readyState == 'complete') {
-      onLoadCallback();
-    }
-    else if (window.addEventListener) {
-     window.addEventListener('load', onLoadCallback, false);
-    } else if (window.attachEvent) {
-      window.attachEvent('onload', onLoadCallback);
-    } else {
-      window.onload = onLoadCallback;
-    }
-
-
+    document.body.appendChild(quiqChatFrame);
   }
 
-  window.addEventListener('load', loadQuiqWebchat, false);
+  if (window.addEventListener) {
+    window.addEventListener('load', loadQuiqWebchat, false);
+  } else if (window.attachEvent) {
+    window.attachEvent('onload', loadQuiqWebchat);
+  } else {
+    window.onload = loadQuiqWebchat;
+  }
 }
 catch(ex) {
   throw new Error('Error loading Quiq Webchat', ex);
