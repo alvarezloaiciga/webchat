940095e7cbd69dc007997aecd188cec42f446dcd
Andrew Jenkins <andrew.jenkins@centricient.com>
2017-09-15 14:30:30 -0600

    Ensure parent page is fully loaded

--- SDK/src/index.js
+++ SDK/src/index.js
@@ -12,32 +12,28 @@ import {setupButtons} from 'managers/ButtonManager';
 import {setQuiqOptions} from './Globals';
 import SDKPrototype from './SdkPrototype';
 
+const bootstrap = () => {
+  try {
+    buildChatIFrame();
+    setupButtons();
+  } catch (e) {
+    displayError(`Quiq: error bootstrapping webchat: ${e}`);
+  }
+};
+
 export const Quiq = (options: {[string]: any}) => {
   setQuiqOptions(buildQuiqObject(options));
 
   // Remove any Quiq keys from localStorage--we only wanted to send them webchat the first iframes were used.
   clearQuiqKeysFromLocalStorage();
 
-  // Defer DOM-related tasks until DOM is fully loaded
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', () => {
-      try {
-        pageSetup();
-      } catch (e) {
-        displayError(`Quiq: error bootstrapping webchat: ${e}`);
-      }
-    });
+  if (document.readyState === 'complete') {
+    bootstrap();
   } else {
-    // document.readyState === 'interactive' | 'loaded'
-    pageSetup();
+    window.addEventListener('load', bootstrap);
   }
 
   return Object.create(SDKPrototype);
-};
-
-const pageSetup = () => {
-  buildChatIFrame();
-  setupButtons();
 };
 
 export default Quiq;
