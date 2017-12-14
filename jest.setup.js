f96c83faf625511d21a6d47d064cf63971df9dc9
Nate Norberg <nate.norberg@goquiq.com>
2017-12-14 13:47:05 -0700

    Moved babel to quiq-scripts

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,7 @@
 import raf from './rafJestPolyfill';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'babel-polyfill';
 
 Enzyme.configure({adapter: new Adapter()});
 
