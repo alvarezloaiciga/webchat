59dc154c0e4466dcf06e135e18e5bf50ab0eaa08
Fred Vollmer <fredvollmer@ip-172-20-0-24.us-west-2.compute.internal>
2018-04-30 10:12:53 -0600

    1.2.22 Fix link to getting started in sdk docs

--- SDK/src/SdkPrototype.js
+++ SDK/src/SdkPrototype.js
@@ -9,7 +9,7 @@ import {setRegistrationFieldValue} from 'Globals';
 /**
  * @description
  * The Quiq object, returned by a call to the `Quiq()` initialization function, exposes methods you can use to interact with the Web Chat UI and the Quiq Web Chat service.
- * For information about the Quiq initialization function, see <a href="/#/?id=adding-chat-to-your-page">Adding chat to your page</a>.
+ * For information about the Quiq initialization function, see <a href="#/?id=adding-chat-to-your-page">Adding chat to your page</a>.
  * ```javascript
  * var chat = Quiq(host: string, contactPoint: string [, options]);
  * ```
